2c00e2f9e711c74c73cb9131cf341ceb34f5b586
Marcus Vinícius da Silveira richa <marcus.richa@sempreceub.com>
2021-11-02 16:55:16 -0300

    Criar a estrutura basica do servidor. Configurar middleware JSON, e importar os modulos de tratamento de erro, not-found.js e error-handler.js, e definir o funcionamento deles com o Express

--- app.js
+++ app.js
@@ -1,3 +1,51 @@
 // https://www.youtube.com/watch?v=qwfE7fSVaZM
 // 3º10'32''
-console.log('04 Store API')
+require('dotenv').config();
+const express = require('express');
+
+const notFound = require('./middleware/not-found');
+const errorMiddleware = require('./middleware/error-handler');
+
+
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+app.get('/', (req, res) => {
+	res.send('<h1>Store API</h1><a href="/api/v1/products">products route</a>');
+});
+
+// Products Route
+
+
+// 404
+app.use(notFound);
+
+// 500
+app.use(errorMiddleware);
+
+
+
+const port = process.env.PORT || 3000;
+
+const start = () => {
+	try {
+		// DB Connection
+		
+		
+		app.listen(port, () => {
+			console.log(`Server listening at port ${port}`);
+		});
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+
+
+start();
+
+
+
